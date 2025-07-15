c92e5d5f0648433474289b5d6d6e1e2d859619ca
pepsipotty <smkkhanz@gmail.com>
2025-07-15 16:19:10 -0400

    Initial files to set the stage

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './layouts/**/*.{html,js}',
+    './content/**/*.{html,js,md}',
+    './public/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} 
